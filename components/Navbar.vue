9dd27e28dfa6ac1f19658b6ada251767fa33301f
martoast <alexmartos96@gmail.com>
2021-04-28 01:33:42 -0700

    navbar for auth

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,21 +1,56 @@
 <template>
-  <v-app-bar :clipped-left="clipped" fixed app>
-    <v-toolbar-title v-text="title" />
-    <v-spacer />
+  <div>
+    <v-navigation-drawer
+      v-if="isAuthenticated"
+      v-model="drawer"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
 
-    <div v-if="isAuthenticated">
-      <a class="navbar-link">
-        {{ loggedInUser.username }}
-      </a>
-      <v-btn text to="/login"> profile </v-btn>
-      <v-btn text to="/register"> log out </v-btn>
-    </div>
+        <v-list-item-content>
+          <v-list-item-title>{{ loggedInUser.username }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-    <div v-if="!isAuthenticated">
-      <v-btn text to="/login"> Login </v-btn>
-      <v-btn text to="/register"> Sign up </v-btn>
-    </div>
-  </v-app-bar>
+      <v-divider></v-divider>
+      <v-list dense>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block @click="logout"> Logout </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+
+      <div v-if="!isAuthenticated">
+        <v-btn text to="/login"> Login </v-btn>
+        <v-btn text to="/register"> Sign up </v-btn>
+      </div>
+    </v-app-bar>
+  </div>
 </template>
 <script>
 import { mapGetters } from 'vuex'
@@ -25,6 +60,14 @@ export default {
     return {
       clipped: false,
       title: 'Virtual Sessions',
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'mdi-view-dashboard', to: '/' },
+        {
+          title: 'Profile',
+          icon: 'mdi-forum',
+        },
+      ],
     }
   },
   computed: {
@@ -54,7 +97,11 @@ export default {
   },
   methods: {
     async logout() {
-      await this.$auth.logout()
+      try {
+        await this.$auth.logout()
+      } catch (e) {
+        console.log(e)
+      }
     },
   },
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app dark>
-    <Navbar />
+    <Navbar> </Navbar>
 
     <v-main>
       <v-container>
@@ -28,28 +28,10 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-    }
-  },
-
-  mounted() {
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(
-      document.querySelectorAll('.navbar-burger'),
-      0
-    )
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-      // Add a click event on each of them
-      $navbarBurgers.forEach((el) => {
-        el.addEventListener('click', () => {
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target
-          const $target = document.getElementById(target)
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active')
-          $target.classList.toggle('is-active')
-        })
-      })
+      items: [
+        { title: 'Home', icon: 'mdi-view-dashboard' },
+        { title: 'About', icon: 'mdi-forum' },
+      ],
     }
   },
 }
